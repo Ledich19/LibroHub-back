dbc3cdca8d30795b33597f59c1974bbdcf80d819
Oleksandr <crm.31885@gmail.com>
2025-05-23 11:20:04 +0300

    fix: fix triger migration

--- drizzle/0001_add_trigers_to_book.sql
+++ drizzle/0001_add_trigers_to_book.sql
@@ -29,26 +29,26 @@ AFTER INSERT OR UPDATE OR DELETE ON book_reviews
 FOR EACH ROW EXECUTE FUNCTION update_book_rating();
 
 -- Функция для обновления рейтинга серии
-CREATE OR REPLACE FUNCTION update_book_series_rating() RETURNS TRIGGER AS $$
+CREATE OR REPLACE FUNCTION update_series_rating() RETURNS TRIGGER AS $$
 BEGIN
   -- Обновляем рейтинг в таблице book_series
   UPDATE book_series
   SET rating = (
     SELECT COALESCE(AVG(value), 0)
-    FROM book_series_ratings
-    WHERE book_series_id = NEW.book_series_id
+    FROM series_ratings
+    WHERE series_id = NEW.series_id
   )
-  WHERE id = NEW.book_series_id;
+  WHERE id = NEW.series_id;
 
   -- Если запись удаляется, обновляем рейтинг для старого book_series_id
   IF TG_OP = 'DELETE' THEN
     UPDATE book_series
     SET rating = (
       SELECT COALESCE(AVG(value), 0)
-      FROM book_series_ratings
-      WHERE book_series_id = OLD.book_series_id
+      FROM series_ratings
+      WHERE series_id = OLD.book_series_id
     )
-    WHERE id = OLD.book_series_id;
+    WHERE id = OLD.series_id;
   END IF;
 
   RETURN NULL;
@@ -56,6 +56,6 @@ END;
 $$ LANGUAGE plpgsql;
 
 -- Триггер для вызова функции после операций INSERT, UPDATE, DELETE
-CREATE TRIGGER book_series_rating_trigger
-AFTER INSERT OR UPDATE OR DELETE ON book_series_ratings
-FOR EACH ROW EXECUTE FUNCTION update_book_series_rating();
+CREATE TRIGGER series_rating_trigger
+AFTER INSERT OR UPDATE OR DELETE ON series_ratings
+FOR EACH ROW EXECUTE FUNCTION update_series_rating();
